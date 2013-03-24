e1700a6c748de0cec0b392db70ed62eace14c1f5
Michael Pustovit <mpustovit@stanfy.com.ua>
2013-03-24 23:36:42 +0200

    GUI fixes

--- src/main/java/edu/kpi/fbp/gui/app/MainWindow.java
+++ src/main/java/edu/kpi/fbp/gui/app/MainWindow.java
@@ -3,10 +3,19 @@ package edu.kpi.fbp.gui.app;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Map;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
+import javax.swing.JTabbedPane;
+import javax.swing.JTextArea;
 import javax.swing.border.EmptyBorder;
 
 import edu.kpi.fbp.gui.file.SaveLoadCore;
@@ -17,72 +26,61 @@ import edu.kpi.fbp.gui.panels.ColorTab;
 import edu.kpi.fbp.gui.panels.ComponentTree;
 import edu.kpi.fbp.gui.panels.DescriptionTab;
 import edu.kpi.fbp.gui.panels.WorkField;
-import edu.kpi.fbp.gui.primitives.Node;
 import edu.kpi.fbp.utils.ComponentsObserver.ComponentClassDescriptor;
-import net.miginfocom.swing.MigLayout;
-import java.awt.Color;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.ArrayList;
-import java.util.Map;
-import javax.swing.JMenuItem;
-import javax.swing.JSplitPane;
-import javax.swing.JTabbedPane;
-import javax.swing.JMenuBar;
-import javax.swing.JMenu;
-import javax.swing.JTextArea;
 
 /** Main application window. */
 public class MainWindow extends JFrame {
 
-  /** SerialVersionUID. */
+  /** GUI constants. */
+  private static final int ROOT_FORM_BORDER = 5;
+/** SerialVersionUID. */
   private static final long serialVersionUID = 1L;
   /** Global window container. */
-  private JPanel contentPane;
+  private final JPanel contentPane;
   /** Hierarchical structure of all existing node types. */
-  private JPanel panelComponentTree;
+  private final JPanel panelComponentTree;
   /** Component palette. */
-  private ComponentTree classComponentTree;
+  private final ComponentTree classComponentTree;
   /** Panel for patchbay of nodes. */
-  private JPanel panelWorkField;
+  private final JPanel panelWorkField;
   /** Class for patchbay of nodes. */
-  private WorkField classWorkField;
+  private final WorkField classWorkField;
   /** Display attributes of single node. */
-  private JPanel panelOption;
+  private final JPanel panelOption;
   /** Console for all text output. */
-  private JPanel panelConsole;
+  private final JPanel panelConsole;
   /** Link to server. */
-  private ServerConnection serverConnection = new LocalConnection();
+  private final ServerConnection serverConnection = new LocalConnection();
   /** Tabed pane consist of three pane : color palette, node attributes, node description. */
-  private JTabbedPane tabbedPane;
+  private final JTabbedPane tabbedPane;
   /** {@link ColorTab}. */
-  private ColorTab colorTab;
+  private final ColorTab colorTab;
   /** {@link AttributeTab}. */
-  private AttributeTab attributeTab;
+  private final AttributeTab attributeTab;
   /** {@link DescriptionTab}. */
-  private DescriptionTab descriptionTab;
+  private final DescriptionTab descriptionTab;
   /** {@link JMenuBar}. */
-  private JMenuBar menuBar;
+  private final JMenuBar menuBar;
   /** Edit scheme. */
-  private JMenu mnEdit;
+  private final JMenu mnEdit;
   /** Save/load scheme. */
-  private JMenu mnFile;
+  private final JMenu mnFile;
   /** Run scheme. */
-  private JMenu mnRun;
+  private final JMenu mnRun;
   /** Run scheme. */
-  private JMenuItem mnRunNetwork;
+  private final JMenuItem mnRunNetwork;
   /** Run scheme with parameters. */
-  private JMenuItem mnRunNetworkParam;
+  private final JMenuItem mnRunNetworkParam;
   /** Save scheme. */
-  private JMenuItem mnFileOpen;
+  private final JMenuItem mnFileOpen;
   /** Save scheme. */
-  private JMenuItem mnFileSave;
+  private final JMenuItem mnFileSave;
   /** Delete choosed cell and all links to it. */
-  private JMenuItem mnEditDelete;
+  private final JMenuItem mnEditDelete;
   /** Clear all field. */
-  private JMenuItem mnEditDeleteAll;
+  private final JMenuItem mnEditDeleteAll;
   /** {@link SaveLoadCore}. */
-  private SaveLoadCore slCore = new SaveLoadCore();
+  private final SaveLoadCore slCore = new SaveLoadCore();
 
   /**
    * Create the frame.
@@ -94,39 +92,39 @@ public class MainWindow extends JFrame {
     descriptionTab = new DescriptionTab();
     attributeTab = new AttributeTab();
     classWorkField = new WorkField(colorTab, descriptionTab, attributeTab);
-    
+
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     setBounds(100, 100, 900, 500);
-    
+
     menuBar = new JMenuBar();
     setJMenuBar(menuBar);
-    
+
     mnFile = new JMenu("File");
     menuBar.add(mnFile);
-    
+
     mnFileOpen = new JMenuItem("Open  (Ctrl+O)");
     mnFileOpen.addActionListener(new ActionListener() {
-      
+
       @Override
-      public void actionPerformed(ActionEvent e) {
+      public void actionPerformed(final ActionEvent e) {
         slCore.load(classWorkField, classComponentTree);
       }
     });
     mnFile.add(mnFileOpen);
-    
+
     mnFileSave = new JMenuItem("Save  (Ctrl+S)");
     mnFileSave.addActionListener(new ActionListener() {
-      
+
       @Override
-      public void actionPerformed(ActionEvent e) {
+      public void actionPerformed(final ActionEvent e) {
         slCore.save(classWorkField.getNodes(), true);
       }
     });
     mnFile.add(mnFileSave);
-    
+
     mnEdit = new JMenu("Edit");
     menuBar.add(mnEdit);
-    
+
     mnEditDelete = new JMenuItem("Delete      (Del)");
     mnEditDelete.addActionListener(new ActionListener() {
       @Override
@@ -135,7 +133,7 @@ public class MainWindow extends JFrame {
       }
     });
     mnEdit.add(mnEditDelete);
-    
+
     mnEditDeleteAll = new JMenuItem("Delete all (Ctrl+Del)");
     mnEditDeleteAll.addActionListener(new ActionListener() {
       @Override
@@ -144,27 +142,27 @@ public class MainWindow extends JFrame {
       }
     });
     mnEdit.add(mnEditDeleteAll);
-    
+
     mnRun = new JMenu("Run");
     menuBar.add(mnRun);
-    
-    mnRunNetwork = new JMenuItem("Run                                  (Ctrl+R)");
+
+    mnRunNetwork = new JMenuItem("Run");
     mnRunNetwork.addActionListener(new ActionListener() {
-      
+
       @Override
-      public void actionPerformed(ActionEvent e) {
+      public void actionPerformed(final ActionEvent e) {
         slCore.save(classWorkField.getNodes(), false);
         serverConnection.networkRun(slCore.getNetworkModel());
         slCore.clean();
       }
     });
     mnRun.add(mnRunNetwork);
-    
+
     mnRunNetworkParam = new JMenuItem("Run with parameters  (Ctrl+P)");
     mnRunNetworkParam.addActionListener(new ActionListener() {
-      
+
       @Override
-      public void actionPerformed(ActionEvent e) {
+      public void actionPerformed(final ActionEvent e) {
         slCore.save(classWorkField.getNodes(), false);
         if (slCore.getParametersStore() != null) {
           serverConnection.networkRun(slCore.getNetworkModel(), slCore.getParametersStore());
@@ -175,106 +173,98 @@ public class MainWindow extends JFrame {
       }
     });
     mnRun.add(mnRunNetworkParam);
-    
+
     contentPane = new JPanel();
-    contentPane.setBackground(Color.WHITE);
-    contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+    contentPane.setBorder(new EmptyBorder(ROOT_FORM_BORDER, ROOT_FORM_BORDER, ROOT_FORM_BORDER, ROOT_FORM_BORDER));
     setContentPane(contentPane);
-    contentPane.setLayout(new BorderLayout());//new MigLayout("", "[::160px,grow][grow][::150px,grow]", "[159.00,grow][::100px,grow]"));
-    
-    panelComponentTree = new JPanel();
-    panelComponentTree.setLayout(new BorderLayout());
+    contentPane.setLayout(new BorderLayout());
+
+    // Component pallet
+    panelComponentTree = new JPanel(new BorderLayout());
     panelComponentTree.add(new JScrollPane(classComponentTree.build()), BorderLayout.CENTER);
-    
-    panelWorkField = new JPanel();
-    panelWorkField.setLayout(new BorderLayout());
+
+    // Working pane
+    panelWorkField = new JPanel(new BorderLayout());
     panelWorkField.add(classWorkField.createMXGraph(), BorderLayout.CENTER);
-    
-    panelOption = new JPanel();
-    panelOption.setBackground(Color.white);
-    panelOption.setLayout(new BorderLayout());
+
+    // Options pane
+    panelOption = new JPanel(new BorderLayout());
     tabbedPane = new JTabbedPane(JTabbedPane.TOP);
-    //Color chooser
+
+    //// Color chooser
     tabbedPane.addTab("Color", colorTab.createPalette(classWorkField.getGraph()));
-    //Component description
+    //// Component description
     tabbedPane.addTab("Description", descriptionTab.getDescriptionPanel());
-    //Attribute panel
+    //// Attribute panel
     tabbedPane.addTab("Parameters", attributeTab.getAttributePanel());
     panelOption.add(tabbedPane, BorderLayout.CENTER);
-    
-    panelConsole = new JPanel();
-    panelConsole.setBackground(Color.WHITE);
-    panelConsole.add(new JTextArea("LOKJLJKBGHCCTVGBKJNM"), BorderLayout.CENTER);
-    
-    JPanel lv0 = new JPanel();
-    lv0.setLayout(new BorderLayout());
-    panelOption.setPreferredSize(new Dimension(150, 350));
-    panelWorkField.setPreferredSize(new Dimension(590, 350));
-    JSplitPane lv0Split = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, panelWorkField, panelOption);
+
+    panelConsole = new JPanel(new BorderLayout());
+    panelConsole.add(new JTextArea(), BorderLayout.CENTER);
+
+    // Level 0: work pane + options panel
+    final JPanel lv0 = new JPanel(new BorderLayout());
+    final JSplitPane lv0Split = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, panelWorkField, panelOption);
     lv0.add(lv0Split, BorderLayout.CENTER);
-    
-    JPanel lv1 = new JPanel();
-    lv1.setLayout(new BorderLayout());
-    panelConsole.setPreferredSize(new Dimension(580, 150));
-    JSplitPane lv1Split = new JSplitPane(JSplitPane.VERTICAL_SPLIT, lv0, panelConsole);
+    lv0Split.setResizeWeight(0.9);
+
+    // Level 1: level 0 + console pane
+    final JPanel lv1 = new JPanel(new BorderLayout());
+    final JSplitPane lv1Split = new JSplitPane(JSplitPane.VERTICAL_SPLIT, lv0, panelConsole);
     lv1.add(lv1Split, BorderLayout.CENTER);
-    
-    JPanel lv2 = new JPanel();
-    lv2.setLayout(new BorderLayout());
-    panelComponentTree.setMaximumSize(new Dimension(160, 500));
-    JSplitPane lv2Split = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, panelComponentTree, lv1);
+    lv1Split.setResizeWeight(0.75);
+
+    // Level 2: level 1 + component pallete
+    final JPanel lv2 = new JPanel(new BorderLayout());
+    final JSplitPane lv2Split = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, panelComponentTree, lv1);
     lv2.add(lv2Split, BorderLayout.CENTER);
+    lv2Split.setResizeWeight(0.1);
 
     contentPane.add(lv2, BorderLayout.CENTER);
-    /*
-    contentPane.add(panelWorkField, "cell 1 0,grow");
-    contentPane.add(panelComponentTree, "cell 0 0 1 2,grow");
-    contentPane.add(panelOption, "cell 2 0,grow");
-    contentPane.add(panelConsole, "cell 1 1 2 1,grow");
-    */
   }
-  
+
   /**
    * Launch the application.
    * @param args dance, checkstyle, dance
    */
-  public static void main(String[] args) {
+  public static void main(final String[] args) {
     EventQueue.invokeLater(new Runnable() {
+      @Override
       public void run() {
         try {
-          MainWindow frame = new MainWindow();
+          final MainWindow frame = new MainWindow();
           frame.setVisible(true);
-        } catch (Exception e) {
+        } catch (final Exception e) {
           e.printStackTrace();
         }
       }
     });
   }
-  
-  /** 
+
+  /**
    * Place in input variables coordinates of WorkField for drag'n'drop.
    * @return int[4] = [x, y, width, heigth]
    */
   public int[] getWorkFieldCoordinates() {
-    int[] res = new int[4];
+    final int[] res = new int[4];
     res[0] = panelWorkField.getLocation().x;
     res[1] = panelWorkField.getLocation().y;
     res[2] = classWorkField.getGraphComponentSize().width;
     res[3] = classWorkField.getGraphComponentSize().height;
-    
+
     return res;
   }
-  
+
   /** @return classWorkField */
   public WorkField getClassWorkField() {
     return classWorkField;
   }
-  
+
   /** @return available node types */
   public Map<String, ComponentClassDescriptor> getAvailableComponents() {
     return serverConnection.getAvailableComponents();
   }
-  
+
   /**
    * Test method used to calculate new node position.
    * @return JPanel size
--- src/main/java/edu/kpi/fbp/gui/panels/WorkField.java
+++ src/main/java/edu/kpi/fbp/gui/panels/WorkField.java
@@ -2,7 +2,6 @@ package edu.kpi.fbp.gui.panels;
 
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Point;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
@@ -21,7 +20,6 @@ import com.mxgraph.view.mxGraph;
 import edu.kpi.fbp.gui.primitives.Link;
 import edu.kpi.fbp.gui.primitives.Node;
 import edu.kpi.fbp.gui.primitives.Port;
-import edu.kpi.fbp.parse.Connection;
 
 
 /** Patchbay of nodes. */
@@ -37,7 +35,7 @@ public class WorkField {
   private mxGraphComponent graphComponent;
   /** Maximum id of nodes. Used for create unique node name. */
   private int maxId = 0;
-  
+
   /** Need to set last choosed node. */
   ColorTab colorTab;
   /** Need to set last choosed node. */
@@ -46,42 +44,42 @@ public class WorkField {
   AttributeTab attributeTab;
   /** Last choosed node. */
   Node lastChoose = null;
-  
+
   /** {@link WorkField}.
    * @param colorTab - used to change last choosed node.
    * @param descriptionTab - used to change last choosed node.
    * @param attributeTab - used to change last choosed node.
    */
-  public WorkField(ColorTab colorTab, DescriptionTab descriptionTab, AttributeTab attributeTab) {
+  public WorkField(final ColorTab colorTab, final DescriptionTab descriptionTab, final AttributeTab attributeTab) {
     this.colorTab = colorTab;
     this.descriptionTab = descriptionTab;
     this.attributeTab = attributeTab;
   }
-  
+
   /** Return and increase maximum id of nodes.
    * @return maximum id of nodes
    */
   public int getMaxId() {
-    int buf = maxId;
+    final int buf = maxId;
     maxId++;
     return buf;
   }
-  
+
   /** @param maxId - maximum id of nodes.*/
-  public void setMaxId(int maxId) {
+  public void setMaxId(final int maxId) {
     this.maxId = maxId;
   }
-  
+
   /** @return mxGraph */
   public mxGraph getGraph() {
     return graph;
   }
-  
+
   /** @return last choosed node. */
   public Node getLastChoosed() {
     return lastChoose;
   }
-  
+
   /** @return graphComponent size in Dimension*/
   public Dimension getGraphComponentSize() {
     return graphComponent.getSize();
@@ -91,9 +89,9 @@ public class WorkField {
    * @param cell - graphical representation of node
    * @return node revelant to cell
    */
-  public Node getNode(Object cell) {
+  public Node getNode(final Object cell) {
 
-    for (Node node : nodes) {
+    for (final Node node : nodes) {
       if (node.getCell().equals(cell)) {
         return node;
       }
@@ -101,60 +99,60 @@ public class WorkField {
 
     return null;
   }
-  
+
   /**
    * @param x - coordinate of node
    * @param y - coordinate of node
    * @return node revelant to coordinates
    */
-  public Node getNode(int x, int y) {
+  public Node getNode(final int x, final int y) {
     return getNode(graphComponent.getCellAt(x, y));
   }
-  
+
   /** @return array of all components on scheme. */
   public ArrayList<Node> getNodes() {
     return nodes;
   }
-  
+
   /** Save array of all components.
    * @param nodes - array of nodes
    */
-  public void setNodes(ArrayList<Node> nodes) {
+  public void setNodes(final ArrayList<Node> nodes) {
     this.nodes = nodes;
   }
-  
+
   /**  Notify tabs about change lastChoosed node.
    * @param node - lastChoosed node
    */
-  public void updateLastChoosed(Node node) {
+  public void updateLastChoosed(final Node node) {
     lastChoose = node;
     colorTab.setNode(lastChoose);
     descriptionTab.createDescription(lastChoose);
     attributeTab.createAttributesPanel(lastChoose);
   }
-  
+
   /** Saving new node and set it to {@link ColorTab.node}.
    * @param newNode - new node (k.o.)
    */
-  public void addNode(Node newNode) {
+  public void addNode(final Node newNode) {
     nodes.add(newNode);
     updateLastChoosed(newNode);
   }
-  
+
   /** Used for debug. Print all nodes. */
   public void showNodes() {
-    for (Node node : nodes) {
+    for (final Node node : nodes) {
       System.out.println("//=======//" + node.toString() + "//=======//");
     }
   }
-  
+
   /**
    * @param cell - graphical representation of port
    * @return port revelant to cell
    */
-  public Port getPort(Object cell) {
-    for (Node node : nodes) {
-      for (Port port : node.getPorts()) {
+  public Port getPort(final Object cell) {
+    for (final Node node : nodes) {
+      for (final Port port : node.getPorts()) {
         if (port.getCell().equals(cell)) {
           return port;
         }
@@ -165,40 +163,40 @@ public class WorkField {
 
   /** Delete choosed cell and all links to it. */
   public void deleteCell() {
-    
+
     if (lastChoose != null) {
-      ArrayList<Object> bufArray = new ArrayList<Object>();
+      final ArrayList<Object> bufArray = new ArrayList<Object>();
       //Delete node
       bufArray.add(lastChoose.getCell());
       //Delete ports
-      for (Port port : lastChoose.getPorts()) {
+      for (final Port port : lastChoose.getPorts()) {
         bufArray.add(port.getCell());
       }
       //Find and delete links to this nodes
-      for (Node node : nodes) {
+      for (final Node node : nodes) {
         //if (node.getLinks() != null) {
-          for (Link link : node.getLinks()) {
+          for (final Link link : node.getLinks()) {
             if (link.getDestinationNodeName().equals(lastChoose.getName())) {
               node.deleteLink(link);
             }
           }
         //}
       }
-      
+
       graph.removeCells(bufArray.toArray());
       updateLastChoosed(null);
     }
   }
-  
+
   /** Clear all field. */
   public void deleteAll() {
     graph.removeCells(graph.getChildVertices(graph.getDefaultParent()));
     nodes.clear();
     maxId = 0;
-    
+
     updateLastChoosed(null);
   }
-  
+
   /**
    * @return new work field.
    */
@@ -208,15 +206,17 @@ public class WorkField {
 
       // Ports are not used as terminals for edges, they are
       // only used to compute the graphical connection point
-      public boolean isPort(Object cell) {
-        mxGeometry geo = getCellGeometry(cell);
+      @Override
+      public boolean isPort(final Object cell) {
+        final mxGeometry geo = getCellGeometry(cell);
 
         return (geo != null) ? geo.isRelative() : false;
       }
 
       // Implements a tooltip that shows the actual
       // source and target of an edge
-      public String getToolTipForCell(Object cell) {
+      @Override
+      public String getToolTipForCell(final Object cell) {
         if (model.isEdge(cell)) {
           return convertValueToString(model.getTerminal(cell, true)) + " -> "
               + convertValueToString(model.getTerminal(cell, false));
@@ -226,59 +226,62 @@ public class WorkField {
       }
 
       // Removes the folding icon and disables any folding
-      public boolean isCellFoldable(Object cell, boolean collapse) {
+      @Override
+      public boolean isCellFoldable(final Object cell, final boolean collapse) {
         return false;
       }
-      
+
       //Disable cell resizing
-      public boolean isCellResizable(Object cell) {
+      @Override
+      public boolean isCellResizable(final Object cell) {
         return false;
       }
 
       /** Новые порты - новые проблемы, вот таким хитрым хреном запрещаем перемешение портов. */
-      public boolean isCellMovable(Object cell) {
+      @Override
+      public boolean isCellMovable(final Object cell) {
         if (!"port".equals(((mxCell)cell).getId())) {
           return true;
         }
         return false;
       }
-    
+
     };
 
     // Sets the default edge style
-    Map<String, Object> style = graph.getStylesheet().getDefaultEdgeStyle();
+    final Map<String, Object> style = graph.getStylesheet().getDefaultEdgeStyle();
     style.put(mxConstants.STYLE_EDGE, mxEdgeStyle.ElbowConnector);
-       
+
     graphComponent = new mxGraphComponent(graph);
-    
+
     graphComponent.getViewport().setOpaque(false);
     graphComponent.setOpaque(true);
     graphComponent.setBackground(Color.WHITE);
-    
+
     //Create listener to save new position of moved cell
     graph.addListener(mxEvent.CELLS_MOVED, new mxIEventListener() {
         @Override
         public void invoke(final Object arg0, final mxEventObject arg1) {
 
           final Object[] cells = (Object[]) arg1.getProperty("cells");
-          Node movedNode = getNode(cells[0]);
-          
+          final Node movedNode = getNode(cells[0]);
+
           if (movedNode != null) {
             final int oldX = movedNode.getPosition().x, oldY = movedNode.getPosition().y;
 
             // Сохраняем новое положение вершины
-            int newX = (int) (oldX + Double.parseDouble("" + arg1.getProperty("dx"))), newY = (int) (oldY + Double.parseDouble("" + arg1.getProperty("dy")));
+            final int newX = (int) (oldX + Double.parseDouble("" + arg1.getProperty("dx"))), newY = (int) (oldY + Double.parseDouble("" + arg1.getProperty("dy")));
             movedNode.setPosition(newX, newY);
 
             // Смещаем порты относительно новой позиции вершины
-            for (Port port : movedNode.getPorts()) {
+            for (final Port port : movedNode.getPorts()) {
               graph.moveCells(new Object[] {port.getCell()}, newX - oldX, newY - oldY);
             }
           }
-          
+
         }
     });
-    
+
     //Create mouse listener to save last clicked node
     graphComponent.getGraphControl().addMouseListener(new MouseAdapter() {
       @Override
@@ -288,7 +291,7 @@ public class WorkField {
         }
       }
     });
-    
+
     //Create listener to save connections
     graphComponent.getConnectionHandler().addListener(mxEvent.CONNECT, new mxIEventListener() {
         @Override
@@ -298,18 +301,18 @@ public class WorkField {
           // Добавляем связь к соедененным обьектам
           // сохраняем в ноде-источнике
           if (edge.getTarget() != null) {
-            Port sourcePort = getPort(edge.getSource()), destinationPort = getPort(edge.getTarget());
-            Link newLink = new Link(sourcePort.getParent().getName(), sourcePort.getName(), destinationPort.getParent().getName(), destinationPort.getName());
+            final Port sourcePort = getPort(edge.getSource()), destinationPort = getPort(edge.getTarget());
+            final Link newLink = new Link(sourcePort.getParent().getName(), sourcePort.getName(), destinationPort.getParent().getName(), destinationPort.getName());
             newLink.setCell(edge, sourcePort.getCell(), destinationPort.getCell());
             sourcePort.getParent().addLink(newLink);
           } else {
             graph.removeCells(new Object[] {edge});
           }
-          
+
           if (debugMode) {
             showNodes();
           }
-          
+
         }
     });
 
